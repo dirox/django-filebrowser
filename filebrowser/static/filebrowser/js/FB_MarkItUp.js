45c9bef9e26885d016e8b66a98a8bd960b033fc0
Clément <clement.nodet@gmail.com>
2011-08-18 12:13:58 +0700

    pass also the filename to MarkItUp editor

--- filebrowser/static/filebrowser/js/FB_MarkItUp.js
+++ filebrowser/static/filebrowser/js/FB_MarkItUp.js
@@ -1,6 +1,6 @@
-function FileSelect(fileUrl) {
+function FileSelect(fileUrl, fileName) {
     var $ = window.top.opener.django.jQuery
-    $($.markItUp.focused).trigger('insertImage', fileUrl)
+    $($.markItUp.focused).trigger('insertImage', [fileUrl, fileName])
     window.top.close()
     window.top.opener.focus()
 }
